8ba9f6fb245d24c2c863116c30cad17458380cd8
James Kiel <jblairkiel@gmail.com>
2022-07-19 00:36:12 -0500

    creating and getting user

--- db/index.js
+++ db/index.js
@@ -1,36 +1,88 @@
 let path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../config.env') })
 const { Pool } = require('pg');
-process.env.CONNECTION_STRING
 const pool = new Pool()
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: '127.0.0.1',
-//     database: 'troubadour',
-//     password: '1',
-//     port: 5432,
-//   })
 pool.connect();
 
 module.exports = {
-    async query(text, params) {
+    async insertQuery(text, params) {
         // invocation timestamp for the query method
-        
         const start = Date.now();
         try {
-            await pool.query(text, params).then(function(res){
+            const result = await pool.query(text, params)
+            const duration = Date.now() - start;
+            console.log(
+            'executed query', 
+            {text, duration, rows: result.rowCount}
+            );
+            return result.rowCount
+            // await pool.query(text, params).then(function(res){
 
-                // time elapsed since invocation to execution
-                const duration = Date.now() - start;
-                console.log(
-                'executed query', 
-                {text, duration, rows: res.rowCount}
-                );
-                return res;
-            }).catch(function(err){
-                console.log(err);
-                return err;
-            });
+            //     // time elapsed since invocation to execution
+            //     const duration = Date.now() - start;
+            //     console.log(
+            //     'executed query', 
+            //     {text, duration, rows: res.rowCount}
+            //     );
+            //     return res;
+            // }).catch(function(err){
+            //     console.log(err);
+            //     return err;
+            // });
+        } catch (error) {
+            console.log('error in query', {text});
+            throw error;
+        }
+    },
+    async updateQuery(text, params) {
+        // invocation timestamp for the query method
+        const start = Date.now();
+        try {
+            const result = await pool.query(text, params)
+            const duration = Date.now() - start;
+            console.log(
+            'executed query', 
+            {text, duration, rows: result.rowCount}
+            );
+            return result.rowCount
+            // await pool.query(text, params).then(function(res){
+
+            //     // time elapsed since invocation to execution
+            //     const duration = Date.now() - start;
+            //     console.log(
+            //     'executed query', 
+            //     {text, duration, rows: res.rowCount}
+            //     );
+            //     return res;
+            // }).catch(function(err){
+            //     console.log(err);
+            //     return err;
+            // });
+        } catch (error) {
+            console.log('error in query', {text});
+            throw error;
+        }
+    },
+
+    async queryGetOne(text, params) {
+        // invocation timestamp for the query method
+        const start = Date.now();
+        try {
+            const queryResult = await pool.query(text, params)
+            return queryResult.rows[0];
+            // await pool.query(text, params).then(function(res){
+
+            //     // time elapsed since invocation to execution
+            //     const duration = Date.now() - start;
+            //     console.log(
+            //     'executed query', 
+            //     {text, duration, rows: res.rowCount}
+            //     );
+            //     return res.rows[0];
+            // }).catch(function(err){
+            //     console.log(err);
+            //     return err;
+            // });
         } catch (error) {
             console.log('error in query', {text});
             throw error;
--- src/user.js
+++ src/user.js
@@ -1,77 +1,80 @@
-import {database as db} from './startup';
-import {TroubadourError} from './helpers';
+import { database as db } from './startup';
+import { TroubadourError } from './helpers';
 const dataDB = require('../db');
 
-function User (userId) {
-    this.userId = userId;
-  
+function User(userId) {
+	this.userId = userId;
+
 }
 
-User.prototype.get = async function() {
-  try{
-    const curUser = await db.troubadourusers.findAll({ where: { user_id: this.userId } });
-    return curUser;
-  } catch (error){
-    console.log(error)
-  }
+User.prototype.get = async function () {
+	try {
+
+		const getResult = await dataDB.queryGetOne(
+			`Select * from Troubadour_Users
+			Where user_id = $1`,
+			[this.userId]
+		)
+		return getResult;
+		//const curUser = await db.troubadourusers.findAll({ where: { user_id: this.userId } });
+		//return curUser;
+	} catch (error) {
+		console.log(error)
+	}
 }
 
-User.prototype.updateLocation = async function(newLocation) {
-    if(!( newLocation instanceof Object) ||
-       ! Number.isFinite(newLocation.lat) ||
-       ! Number.isFinite(newLocation.long)) {
-         throw new TroubadourError('The entered location is invalid. ' +
-         'See docs at https://api.troubadour.tk/docs. ', 400);
-    }
+User.prototype.updateLocation = async function (newLocation) {
+	if (!(newLocation instanceof Object) ||
+		!Number.isFinite(newLocation.lat) ||
+		!Number.isFinite(newLocation.long)) {
+		throw new TroubadourError('The entered location is invalid. ' +
+			'See docs at https://api.troubadour.tk/docs. ', 400);
+	}
+	try {
+		const { updateResult } = await dataDB.updateQuery(
+			`UPDATE Troubadour_Users
+			set updated_at = '$1', last_location = '$2'
+			where user_id = '$3'`,
+			[Date.now(), newLocation, this.userId]
+		);
+		return updateResult;
+	} catch (err) {
+		console.log(err)
+		return err;
+	}
+	// const user = db.troubadourusers.build({ 
+	//   user_id: this.userId,
+	//   updated_at: db.sequelize.fn('NOW'),
+	//   last_location: newLocation
+	//  }).save();
+	// console.log(user.toJSON()); // This is good!
+	// console.log(JSON.stringify(user, null, 4)); // This is also good!
 
-    const user = db.troubadourusers.build({ 
-      user_id: this.userId,
-      updated_at: db.sequelize.fn('NOW'),
-      last_location: newLocation
-     }).save();
-    console.log(user.toJSON()); // This is good!
-    console.log(JSON.stringify(user, null, 4)); // This is also good!
+	// await db.TroubadourUser.upsert({
+	//     user_id: this.userId,
+	//     updated_at: db.sequelize.fn('NOW'),
+	//     last_location: newLocation,
+	// });
 
-    // await db.TroubadourUser.upsert({
-    //     user_id: this.userId,
-    //     updated_at: db.sequelize.fn('NOW'),
-    //     last_location: newLocation,
-    // });
+	//return true;
+}
 
-    return true;
-  }
-
-User.prototype.create = async function() {
-    try{
-        const { rows } = await dataDB.query(
-          `INSERT INTO troubadourusers(user_id, spotify_id, last_location, updated_at) 
+User.prototype.create = async function () {
+	try {
+		const insertResult = await dataDB.insertQuery(
+			`INSERT INTO Troubadour_Users(user_id, spotify_id, last_location, updated_at) 
               VALUES ($1, $2, $3, $4)`,
-              [this.userId, this.userId, null, null]
-          );       
-          return rows; 
-      // db.sequelize.sync();
-      // const a = await db.troubadourusers.create({ 
-      //   user_id: this.userId,
-      //   updated_at: db.sequelize.fn('NOW')
-      //  }).then( result => {
-  
-      //     console.log(result.toJSON()); // This is good!
-      //     console.log(JSON.stringify(result, null, 4)); // This is also good!
-      //   }
-  
-      //  ).catch( err => {
-      //   console.log(err);
-      //  });
-       //return a;
-    } catch(err){
-      return null;
-    }
-  //   return await db.TroubadourUser.upsert({
-  //     user_id: this.userId,
-  //     updated_at: db.sequelize.fn('NOW'),
-  //   }).then(function(test){
-  //     console.log(test);//test returned here as true or false how can i get the inserted id here so i can insert data in other tables using this new id?
-  // });
-  }
+			[this.userId, this.userId, null, null]
+		)
+		return insertResult;
+		// .then(res => {
+		// 	console.log(res.rows[0])
+		// 	return res
+		// 	// { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+		// });
+	} catch (err) {
+		return err;
+	}
+}
 
 module.exports = User;
